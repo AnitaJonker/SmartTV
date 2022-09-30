4f4e35b124c6822d31e5230e05e56406c413183a
Anita <anitaroneljonker@gmail.com>
2022-09-30 14:21:41 +0200

    create template for application and setup security

---
+++ smarty/.fttemplates/createComponent/[FTName]Styles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button } from '../../globalStyles';
+
+export const [FTName] = styled.section`
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  padding-top: clamp(70px, 25vh, 220px);
+  box-shadow: inset 0 0 0 1000px rgba (0, 0, 0, 0.2);
+`;
