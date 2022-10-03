9070a0dd45a8eb5382602e78bd13610c7bad4234
Anita <anitaroneljonker@gmail.com>
2022-10-03 17:34:41 +0200

    changes-needed-for-tailwindcss

--- smarty/src/App.js
+++ smarty/src/App.js
@@ -1,7 +1,11 @@
 // import logo from './logo.svg';
-import './App.css';
-import GetImages from './components/GetImages/GetImages';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css"
+import "./input.css"
+import Grid from "./components/Grid/Grid"
+import Home from "./components/Home/Home"
+import Options from "./components/Options/Options"
+import Fireship from "./components/Fireship/Fireship"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 //console.log(secureObject);
 export default function App() {
@@ -9,9 +13,12 @@ export default function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<GetImages />}></Route>
+          <Route path='/Home' element={<Home />}></Route>
+          <Route path='/' element={<Fireship />}></Route>
+          <Route path='/Options' element={<Options />}></Route>
+          <Route path='/Grid' element={<Grid />}></Route>
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
